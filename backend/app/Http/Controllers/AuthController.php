9cc309e4f9ce89ee4f4da4a1b8ac239707f717f3
inv-athul-krishna-s <athul.s@innovaturelabs.com>
2025-08-07 11:26:22 +0530

    user authentication

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -26,23 +26,46 @@ class AuthController extends Controller
             'password' => Hash::make($request->password),
             'role' => $request->role,
         ]);
+        //Checking admin exist in the db. if exist error occured
         if($request->role == 'admin'){
             if($adminExists){
                 return response()->json(['error' => 'Admin already exists'], 400);
             }
         }
+        elseif (!in_array($request->role,['student','teacher'])) {
+            return  response()->json(['error'=> 'invalid role'],400);
+        }
 
-        return response()->json(['message' => 'User registered successfully'], 201);
+
+        $token=JWTAuth::fromUser($user);
+        
+        return response()->json(['accesstoken'=>$token,'user'=>$user], 201);
     }
-
+    //user login
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
 
         if (!$token = JWTauth::attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['error' => 'invalid credentials'], 401);
         }
 
-        return response()->json(['token' => $token]);
+        return response()->json(['accesstoken' => $token,'user'=>auth()->user(),'role'=>auth()->user()->role]);
+    }
+
+    //user logout
+    public function logout(){
+        try {
+            auth()->logout();
+            return response()->json(['message' => 'Successfully Logged Out']);
+        } catch (Exception $e) {
+            return response()->json(['error' => 'Failed to logout, try again'], 500);
+        }
+    }
+
+    //current user
+    public function me()
+    {
+        return response()->json(auth()->user());
     }
 }
--- backend/database/migrations/2025_08_06_095629_add_role_to_users_table.php
+++ backend/database/migrations/2025_08_06_095629_add_role_to_users_table.php
@@ -1,28 +1,26 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
+class AddRoleToUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-        $table->string('role')->default('student'); // 'student' or 'teacher'
-    });
+            if (!Schema::hasColumn('users', 'role')) {
+                $table->string('role')->default('student');
+            }
+        });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-        $table->dropColumn('role');
-    });
+            if (Schema::hasColumn('users', 'role')) {
+                $table->dropColumn('role');
+            }
+        });
     }
-};
+}
