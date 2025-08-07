c6512fc33a6cfe039c2e13670412c7c916322084
inv-athul-krishna-s <athul.s@innovaturelabs.com>
2025-08-07 09:41:12 +0530

    migrations

--- backend/database/migrations/2025_08_06_095629_add_role_to_users_table.php
+++ backend/database/migrations/2025_08_06_095629_add_role_to_users_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        $table->string('role')->default('student'); // 'student' or 'teacher'
+    });
     }
 
     /**
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
-        });
+        $table->dropColumn('role');
+    });
     }
 };
